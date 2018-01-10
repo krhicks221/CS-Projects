2318d6b45a011923c1223e4a451fcac3a6aac710
Kaytlyn <kaytierose2000@yahoo.com>
2018-01-10 11:27:11 -0500

    Woot Woot!

---
+++ userinput.py
@@ -0,0 +1,7 @@
+#This is a comment. It will not run with the program.
+print('Sometimes you want to get input from a  user.')
+print('Luckily, there is a function for that!')
+print("So, what's your name?")
+name = input()
+print('So, your name is ' + name + '?')
+print('Sounds weird...')
