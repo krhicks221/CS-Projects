7836827809a1a3a026bfb92d4df0330f14425cab
Kaytlyn <kaytierose2000@yahoo.com>
2018-01-15 21:50:01 -0500

    Homework

---
+++ Homework.py
@@ -0,0 +1,20 @@
+math = "death"
+print(math)
+len(math)
+math.upper()
+
+numbers = [1, 3, 5, 6]
+for num in numbers:
+	print(1)
+	if num is 5:
+		print(2)
+		break
+	else:
+		print(3)
+
+def math(death):
+	if type(death) == int or type(death) == float:
+		return abs(death)
+	else:
+		return "Fire!"
+
