a3490c8c2c4a02a4446cb1b4daaf90f5f4536073
Kaytlyn <kaytierose2000@yahoo.com>
2018-01-23 11:14:54 -0500

    Caesar Cipher

---
+++ CAESAR.py
@@ -0,0 +1,27 @@
+def shift(num, shifted):
+	lower = 'abcdefghijklmnopqrstuvwxyz'
+	upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+	aftershift = ''
+	for i in range(len(shifted)):
+		didshift = False
+		for j in range(len(lower)):
+			if lower[j] == shifted[i]:
+				aftershift += lower[(j+num)%26]
+				didshift = True
+				break
+			elif upper[j] == shifted[i]:
+				aftershift += upper[(j+num)%26]
+				didshift = True
+				break
+		if not didshift:
+			aftershift += shifted[j]
+	return aftershift
+
+theshift = 13
+print('Type quit to exit')
+
+while(True):
+	shifted = input()
+	if shifted == 'quit':
+		break
+	print(shift(theshift,shifted))
